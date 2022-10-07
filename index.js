f48e47ff956083796147ce5ca02cf42afa48a8f0
Martins Richard <112550418+Martcode123@users.noreply.github.com>
2022-10-07 01:55:33 +0100

    finally

--- index.js
+++ index.js
@@ -1,34 +1,89 @@
 // https://www.omdbapi.com/?apikey=7566c271&s=fast
+let searchResultEl = document.querySelector(".movie__title");
 const moviesWrapperEl = document.querySelector(".movies__wrapper");
-const searchResultEL = document.querySelector(".movie__title");
-const movieWrapper = document.querySelector(".movies__wrapper");
-
-async function renderMovies() {
-  const movies = await fetch("https://www.omdbapi.com/?apikey=7566c271&s=fast");
+async function onSearchChange(event) {
+  const movie = event.target.value
+  const movies = await fetch(`https://www.omdbapi.com/?apikey=7566c271&s=${movie}`);
   const moviesData = await movies.json();
-  const firstSix =  moviesData.Search.filter((elem) => elem).slice(0, 6);  
-//   searchResultEl.innerHTML = 
-  console.log(firstSix)
-  .map(
-   (movie) =>  `<div class="movie">
-    <figure class="movie__img--wrapper">
-    <img class="movie__img" src="./js assets/logo.svg" alt="" />
-    </figure>
-    <div class="movie__description">
-    <h2 class="movie__title">000000</h2>
-    <h3 class="movie__date">000000</h3>
-    <a
-        href="https://imdb.com/title/.website"
-        class="imdb"
-        target="_blank"
-        >IMDB</a
-    >
-    </div>
-</div>`
+  const firstSix = moviesData.Search.filter((elem) => elem).slice(0, 6);
+  let moviesHTML = firstSix;
+  if (filter === 'NEWEST') {
+     moviesHTML.sort((a, b) => b.Year - a.Year)
+   }
+   else if (filter === 'OLDEST') {
+    moviesHTML.sort((a, b) => a.Year - b.Year)
+   }
+   else if (filter === 'A-Z') {
+    let test = firstSix.sort((a, b) => a.Title.localeCompare(b.Title))
+    console.log(test)
+   }
+  
+  searchResultEl.innerHTML = moviesHTML
+    .map((movie) => {
+    return `<div class="movie">
+   <figure class="movie__img--wrapper">
+      <img class="movie__img" src="${movie.Poster}" alt="" />
+   </figure>
+   <div class="movie__description">
+     <h2 class="movie__title">${movie.Title}</h2>
+     <h3 class="movie__date">${movie.Year}</h3>
+     <a
+     href="https://imdb.com/title/${movie.imdbID}"
+     class="imdb"
+     target="_blank"
+     >IMDB</a
+     >
+   </div>
+ </div>`;
+    })
 
-  )
-  .join("")
+    .join("");
+  
 }
-renderMovies();
+async function renderMovies(filter) {
+  const movies = await fetch(`https://www.omdbapi.com/?apikey=7566c271&s=${movie}`);
+  const moviesData = await movies.json();
+  const firstSix = moviesData.Search.filter((elem) => elem).slice(0, 6);
+  let moviesHTML = firstSix;
+  if (filter === 'NEWEST') {
+     moviesHTML.sort((a, b) => b.Year - a.Year)
+   }
+   else if (filter === 'OLDEST') {
+    moviesHTML.sort((a, b) => a.Year - b.Year)
+   }
+   else if (filter === 'A-Z') {
+    let test = firstSix.sort((a, b) => a.Title.localeCompare(b.Title))
+    console.log(test)
+   }
+  
+  searchResultEl.innerHTML = moviesHTML
+    .map((movie) => {
+    return `<div class="movie">
+   <figure class="movie__img--wrapper">
+      <img class="movie__img" src=".website" alt="" />
+   </figure>
+   <div class="movie__description">
+     <h2 class="movie__title">${movie.Title}</h2>
+     <h3 class="movie__date">${movie.Year}</h3>
+     <a
+     href="https://imdb.com/title/${movie.imdbID}"
+     class="imdb"
+     target="_blank"
+     >IMDB</a
+     >
+   </div>
+ </div>`;
+    })
 
+    .join("");
+  
+   
+}
+
+function filterMovies(event) {
+  renderMovies(event.target.value);
+}
+setTimeout(() => {
+  renderMovies();
+});
 
